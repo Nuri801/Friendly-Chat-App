e7ca0610ce332b84bbebc6a4c162e753df5b8006
Nuri <nuriddin7612@mail.ru>
2022-05-08 23:43:55 +0900

    Friendly Chat App

--- lib/main.dart
+++ lib/main.dart
@@ -130,7 +130,7 @@ class _ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
                 },
                 onSubmitted: _isComposing ? _handleSubmitted : null,
                 decoration: const InputDecoration.collapsed(
-                    hintText: 'Type your message'),
+                    hintText: 'Do not type'),
                 focusNode: _focusNode,
               ),
             ),
